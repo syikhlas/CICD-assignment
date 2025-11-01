d19aa70efa858846e6be575fec30c01a63288dfa
syikhlas <ikhlasatwork@gmail.com>
2025-10-31 21:13:25 -0500

    Fix flake8 E302/E305 in app and tests

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/message', methods=['GET'])
+
+@app.route('/api/message')
 def get_message():
-    return jsonify({'message': 'Hello from Flask!'}), 200
+    return jsonify({'message': 'Hello from Flask!'})
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 from app import app
 
+
 def test_get_message():
     with app.test_client() as client:
         response = client.get('/api/message')
